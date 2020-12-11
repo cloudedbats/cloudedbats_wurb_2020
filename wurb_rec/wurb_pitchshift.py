a6610fc5bd4ef9b5d0ab7ee5ad377f7dd5fc35d1
Arnold Andreasson <aea@mellifica.se>
2020-12-11 22:22:34 +0100

    Formatted with black.

--- wurb_rec/wurb_pitchshift.py
+++ wurb_rec/wurb_pitchshift.py
@@ -13,10 +13,7 @@ import sys
 import time
 import soundfile
 import pathlib
-<<<<<<< HEAD
 
-=======
->>>>>>> f6049a7bc19d5c7f89b28675f6a8c91d92ff4701
 # import threading
 
 
@@ -49,7 +46,6 @@ class WurbPitchShift(object):
         self.to_out_limit = None
         self.out_buffer = None
 
-<<<<<<< HEAD
     def startup(
         self,
         sampling_freq=384000,
@@ -59,17 +55,8 @@ class WurbPitchShift(object):
         filter_high_limit_hz=120000,
         filter_order=10,
         overlap_in_factor=1.5,
-        kaiser_beta=14):
-=======
-    def setup(self, sampling_freq = 384000, 
-                    pitch_div_factor = 10, 
-                    volume = 1.0, 
-                    filter_low_limit_hz = 15000, 
-                    filter_high_limit_hz = 120000, 
-                    filter_order = 10, 
-                    overlap_in_factor = 1.5, 
-                    kaiser_beta = 14):
->>>>>>> f6049a7bc19d5c7f89b28675f6a8c91d92ff4701
+        kaiser_beta=14,
+    ):
         """ """
         try:
             self.sampling_freq_in = sampling_freq
@@ -86,22 +73,16 @@ class WurbPitchShift(object):
             # Buffers.
             self.window_function = numpy.kaiser(self.window_size, beta=kaiser_beta)
             self.in_buffer = numpy.array([], dtype=numpy.float64)
-<<<<<<< HEAD
             self.pitchshift_buffer = numpy.zeros(
                 self.sampling_freq_out, dtype=numpy.float64
             )  # 1 sec.
             self.to_out_limit = int(
                 self.sampling_freq_in / pitch_div_factor / 2
             )  # About 0.5 sec.
-=======
-            self.pitchshift_buffer = numpy.zeros(self.sampling_freq_out, dtype=numpy.float64) # 1 sec.
-            self.to_out_limit = int(self.sampling_freq_in / pitch_div_factor / 2) # About 0.5 sec.
->>>>>>> f6049a7bc19d5c7f89b28675f6a8c91d92ff4701
             self.out_buffer = numpy.array([], dtype=numpy.float64)
         except Exception as e:
             print("Exception: WurbPitchShift: setup:", e)
 
-<<<<<<< HEAD
     def shutdown(self):
         """ """
         # await self.stop()
@@ -119,23 +100,12 @@ class WurbPitchShift(object):
             fs=self.sampling_freq_in,
             output="sos",
         )
-=======
-    def add_buffer(self, buffer):
-        """ """
-        # Filter buffer. Butterworth bandpass.
-        sos = scipy.signal.butter(self.filter_order, 
-                                  [self.filter_low_limit_hz, self.filter_high_limit_hz], 
-                                  btype='bandpass', 
-                                  fs=self.sampling_freq_in, 
-                                  output="sos")
->>>>>>> f6049a7bc19d5c7f89b28675f6a8c91d92ff4701
         filtered = scipy.signal.sosfilt(sos, buffer)
         # Concatenate with old buffer.
         self.in_buffer = numpy.concatenate((self.in_buffer, filtered))
         # Add overlaps on pitchshift_buffer. Window function is applied on "part".
         insert_pos = 0
         while len(self.in_buffer) > self.window_size:
-<<<<<<< HEAD
             part = self.in_buffer[: self.window_size] * self.window_function
             self.in_buffer = self.in_buffer[self.hop_in_length :]
             self.pitchshift_buffer[insert_pos : insert_pos + self.window_size] += part
@@ -157,49 +127,23 @@ class WurbPitchShift(object):
             insert_pos : insert_pos + self.window_size
         ]
         self.pitchshift_buffer[self.window_size :] = 0.0
-=======
-            part = self.in_buffer[:self.window_size] * self.window_function
-            self.in_buffer = self.in_buffer[self.hop_in_length:]
-            self.pitchshift_buffer[insert_pos:insert_pos+self.window_size] += part
-            insert_pos += self.hop_out_length
-            if insert_pos > self.to_out_limit:
-                self.out_buffer = numpy.concatenate((self.out_buffer, self.pitchshift_buffer[:insert_pos]))
-                self.pitchshift_buffer[:self.window_size] = self.pitchshift_buffer[insert_pos:insert_pos+self.window_size]
-                self.pitchshift_buffer[self.window_size:] = 0.0
-                insert_pos = 0
-        # Flush.
-        self.out_buffer = numpy.concatenate((self.out_buffer, self.pitchshift_buffer[:insert_pos]))
-        self.pitchshift_buffer[:self.window_size] = self.pitchshift_buffer[insert_pos:insert_pos+self.window_size]
-        self.pitchshift_buffer[self.window_size:] = 0.0
->>>>>>> f6049a7bc19d5c7f89b28675f6a8c91d92ff4701
         insert_pos = 0
 
     def stream_sound(self):
         """ """
-<<<<<<< HEAD
 
-=======
->>>>>>> f6049a7bc19d5c7f89b28675f6a8c91d92ff4701
         def audio_out_callback(outdata, frames, cffi_time, status):
             """ Locally defined callback. Called from another thread. """
             if status:
                 print("DEBUG: stream_sound/callback: ", status, file=sys.stderr)
-<<<<<<< HEAD
             #
-=======
-            # 
->>>>>>> f6049a7bc19d5c7f89b28675f6a8c91d92ff4701
             if len(self.out_buffer) > frames:
                 data = self.out_buffer[:frames]
                 self.out_buffer = self.out_buffer[frames:]
                 data *= self.volume
                 data = data.reshape(-1, 1)
                 outdata[:] = data
-<<<<<<< HEAD
             else:
-=======
-            else: 
->>>>>>> f6049a7bc19d5c7f89b28675f6a8c91d92ff4701
                 # Send zeroes if out buffer is empty.
                 outdata[:] = numpy.zeros((frames, 1), dtype=numpy.float64)
 
@@ -213,20 +157,11 @@ class WurbPitchShift(object):
             # Start streaming of sound.
             # self.asyncio_loop = asyncio.get_event_loop()
             stream = sounddevice.OutputStream(
-<<<<<<< HEAD
                 device="Built-in Output",  # self.device_name,
                 samplerate=int(self.sampling_freq_out),
                 channels=1,
                 blocksize=0,  # Automatically by ALSA.
                 callback=audio_out_callback,  # Locally defined above.
-=======
-                device= "Built-in Output", # self.device_name,
-                samplerate=int(self.sampling_freq_out),
-                channels=1,
-                blocksize=0, # Automatically by ALSA.
-                callback=audio_out_callback, # Locally defined above.
-
->>>>>>> f6049a7bc19d5c7f89b28675f6a8c91d92ff4701
                 # finished_callback=event.set,
             )
             with stream:
@@ -237,10 +172,6 @@ class WurbPitchShift(object):
             print("Exception stream_sound: ", e)
 
 
-<<<<<<< HEAD
-=======
-
->>>>>>> f6049a7bc19d5c7f89b28675f6a8c91d92ff4701
 if __name__ == "__main__":
     """ """
     pitchshift = WurbPitchShift(wurb_manager=None)
@@ -248,7 +179,6 @@ if __name__ == "__main__":
     for file_path in sorted(pathlib.Path("TEST_PITCHSHIFT").glob("*.wav")):
         print("\nFile: ", str(file_path))
         wav_data_in, fs = soundfile.read(file_path, dtype="float64")
-<<<<<<< HEAD
         pitchshift.startup(
             sampling_freq=fs,
             pitch_div_factor=10,
@@ -256,13 +186,5 @@ if __name__ == "__main__":
             filter_low_limit_hz=15000,
             filter_high_limit_hz=90000,
         )
-=======
-        pitchshift.setup(sampling_freq = fs, 
-                         pitch_div_factor = 10, 
-                         volume = 0.5,
-                         filter_low_limit_hz = 15000, 
-                         filter_high_limit_hz = 90000, 
-                         )
->>>>>>> f6049a7bc19d5c7f89b28675f6a8c91d92ff4701
         pitchshift.add_buffer(wav_data_in)
         pitchshift.stream_sound()
